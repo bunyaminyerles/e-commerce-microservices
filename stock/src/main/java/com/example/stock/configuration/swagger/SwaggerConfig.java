e07a0866b00f03a20f0f98ed018c8e8d2ae5d816
bunyaminyerles <bnymnyrls@hotmail.com>
2023-12-08 13:52:33 +0300

    it adds server of the swaggers

--- stock/src/main/java/com/example/stock/configuration/swagger/SwaggerConfig.java
+++ stock/src/main/java/com/example/stock/configuration/swagger/SwaggerConfig.java
@@ -4,15 +4,29 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final ServerProperties serverProperties;
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        List<Server> server = new ArrayList<>();
+        server.add(new Server().url("http://localhost:" + serverProperties.getPort().toString()));
+
         return new OpenAPI()
+                .servers(server)
                 .info(new Info()
                         .title("Stock API başlık")
                         .version("1.0")
